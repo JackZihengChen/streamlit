ce6028a922683a4723e04b850322591aad9e71a6
Jack <79237806+JackZihengChen@users.noreply.github.com>
2022-08-31 15:46:35 -0500

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-for i in range(500):
+for i in range(5):
   st.write("Here's our first attempt at using data to create a table:")
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
